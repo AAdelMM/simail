565ba0c1a7b83d47e6a43dbe95de7294773b4734
AAdelMM <ahmed.adel426@gmail.com>
2024-07-10 15:25:28 +0300

    merge

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="h-[100vh] w-[100vw] flex justify-center items-center">
+    <h1 class="text-[5rem] text-blue-500">AL SIMAIL CONSTRACTIONS</h1> 
+    </div>
+   <
+</body>
+</html>
