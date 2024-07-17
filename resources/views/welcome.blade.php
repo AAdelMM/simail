f5dfba498bf9e6e2f5e7793c53369aada44a8cb5
AAdelMM <ahmed.adel426@gmail.com>
2024-07-17 12:13:30 +0300

    testing push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
     <div class="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
     <h1 class="text-[5rem] text-yellow-500">AL SIMAIL CONSTRACTIONS</h1> 
     <h2 class="text-[3rem] text-white">The Website Under Establish</h2>
+    <p>Starting again</p>
     </div>
    <
 </body>
