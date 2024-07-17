9b61ca98e7eae9697c13af0bbcfb76ceef5f4b82
AAdelMM <ahmed.adel426@gmail.com>
2024-07-17 22:54:45 +0300

    fav icon

--- resources/views/en/layouts/master.blade.php
+++ resources/views/en/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AL Simail Co</title>
+    <link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <script src="{{ asset('js/main.js') }}"></script>
