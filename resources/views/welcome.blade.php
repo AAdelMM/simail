9b65ac1a439e824333d8c8b8f992d3dafcdad89a
AAdelMM <ahmed.adel426@gmail.com>
2024-07-17 12:27:33 +0300

    more test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,22 @@
     <div class="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
     <h1 class="text-[5rem] text-yellow-500">AL SIMAIL CONSTRACTIONS</h1> 
     <h2 class="text-[3rem] text-white">The Website Under Establish</h2>
-    <p>Starting again</p>
+    
+    <div class="text-[1.2rem] font-bold">
+        main page 
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>our vision</li>
+            <li>our mission</li>
+            <li>Services</li>
+            <li>Parteners</li>
+            <li>clinents</li>
+
+        </ul>
     </div>
+    </div>
+
    <
 </body>
 </html>
