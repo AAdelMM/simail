fb0ae63f5a3de7eb8d5a24c42740f4a81568e090
AAdelMM <ahmed.adel426@gmail.com>
2024-08-05 21:53:02 +0300

    add arabic and edit footer

--- resources/views/ar/sections/footer.blade.php
+++ resources/views/ar/sections/footer.blade.php
@@ -1,53 +1,53 @@
-<section id="footer" class="border-t-2 border-[#efc02e]">
-    <div class="container m-auto flex flex-col  ">
-        <div class="w-[100%] h-[30rem] "><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3571.975241458224!2d50.056896496789555!3d26.456526999999987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49fc2b1281457b%3A0x90faefdeb5c0eec6!2z2YXYpNiz2LPYqSDYp9mE2LPZhdin2LnZitmEINmE2YTZhdmC2KfZiNmE2KfYqg!5e0!3m2!1sen!2sbh!4v1721554400346!5m2!1sen!2sbh" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+<section id="footer" class="border-t-2 border-[#efc02e]" dir="rtl">
+    <div class="container m-auto flex flex-col">
+        <div class="w-[100%] h-[30rem]"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3571.975241458224!2d50.056896496789555!3d26.456526999999987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49fc2b1281457b%3A0x90faefdeb5c0eec6!2z2YXYpNiz2LPYqSDYp9mE2LPZhdin2LnZitmEINmE2YTZhdmC2KfZiNmE2KfYqg!5e0!3m2!1sen!2sbh!4v1721554400346!5m2!1sen!2sbh" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
 
         <div class="w-[100%] h-[20rem] inline-flex flex-wrap bg-[#efc02e] relative justify-around items-center">
 
-            <div class="LS md:w-[30%] w-[50%] h-[100%] flex flex-col justify-center items-center md:m-auto ml-[-45px] ">
-                <div class="w-[60%] text-[#1e304e] md:text-[1.5rem] text-[1rem] font-bold  md:py-5 py-1">
-                    <h1>Office</h1>
-                </div>
-                <div class="w-[60%] text-[#1e304e] ">
-                    <p class="font-bold py-1">Address: <br><span class="font-normal">Damam Prince Nayef Bin Abdulaziz RD</span></p>
-                    <p class="font-bold py-1">Phone: <span class="font-normal">+966 11 400 0085</span></p>
-                    <p class="font-bold py-1">Email:  <span class="font-normal">admin@admin.com</span></p>
-
+            <div class="RS w-[30%] h-[100%] m-auto flex flex-col md:justify-around justify-end items-center">
+                <div class="logo md:w-[20%] w-[50%] h-[10%]"><img src="{{asset('images/logo.png')}}" alt="شعار"></div>
+                <div class="pt-10"><h1 class="md:text-[1.5rem] text-[0.9rem]">تابعنا</h1></div>
+                
+                <div class="social md:w-[50%] w-[90%] flex flex-row justify-around pb-5 md:pb-0 text-white">
+                    <div class="facebook w-[2rem] h-[auto]"><img src="{{asset('images/facebook.png')}}" alt="فيسبوك"></div>
+                    <div class="instagram w-[2rem] h-[auto]"><img src="{{asset('images/insta.png')}}" alt="انستغرام"></div>
+                    <div class="X w-[2rem] h-[auto]"><img src="{{asset('images/x.png')}}" alt="إكس"></div>
+                    <div class="youtube w-[2rem] h-[auto]"><img src="{{asset('images/youtube.png')}}" alt="يوتيوب"></div>
+                    <div class="tiktok w-[2rem] h-[auto]"><img src="{{asset('images/tiktok.png')}}" alt="تيك توك"></div>
                 </div>
             </div>
 
-            <div class="MS w-[30%]  text-[#1e304e] md:text-[2rem] text-[1rem] flex flex-col items-center ">
+            <div class="MS w-[30%] text-[#1e304e] md:text-[2rem] text-[1rem] flex flex-col items-center">
                 <div class="font-bold">
-                    <h1 >Contact Us</h1>
+                    <h1>اتصل بنا</h1>
                 </div>
                 <div class="w-[40%] h-[auto] inline-flex justify-center gap-5 my-5">
                     <div class="w-[2rem] h-[auto]">
-                        <img src="{{asset('images/call.png')}}" alt="call us">
+                        <img src="{{asset('images/call.png')}}" alt="اتصل بنا">
                     </div>
                     <div class="w-[2rem] h-[auto]">
-                        <img src="{{asset('images/message.png')}}" alt="send message">
+                        <img src="{{asset('images/message.png')}}" alt="أرسل رسالة">
                     </div>
                 </div>
             </div>
-            <div class="RS w-[30%] h-[100%] m-auto flex flex-col md:justify-around justify-end items-center ">
-                <div class="logo md:w-[20%] w-[50%] h-[10%] "><img src="{{asset('images/logo.png')}}" alt="logo"></div>
-                <div class="pt-10"><h1 class="md:text-[1.5rem] text-[0.9rem] ">Follow Us</h1></div>
-                
-                <div class="social md:w-[50%] w-[90%] flex flex-row justify-around pb-5 md:pb-0 text-white">
-                    <div class="facebook w-[2rem] h-[auto]"><img src="{{asset('images/facebook.png')}}" alt="facebook"></div>
-                    <div class="instagram w-[2rem] h-[auto]"><img src="{{asset('images/insta.png')}}" alt="insta"></div>
-                    <div class="X w-[2rem] h-[auto]"><img src="{{asset('images/x.png')}}" alt="logo"></div>
-                    <div class="youtube w-[2rem] h-[auto]"><img src="{{asset('images/youtube.png')}}" alt=""></div>
-                    <div class="tiktok w-[2rem] h-[auto]"><img src="{{asset('images/tiktok.png')}}" alt="tiktok"></div>
+
+            <div class="LS md:w-[30%] w-[50%] h-[100%] flex flex-col justify-center items-center md:m-auto mr-[-45px]">
+                <div class="w-[60%] text-[#1e304e] md:text-[1.5rem] text-[1rem] font-bold md:py-5 py-1">
+                    <h1>المكتب</h1>
+                </div>
+                <div class="w-[60%] text-[#1e304e]">
+                    <p class="font-bold py-1">العنوان: <br><span class="font-normal">الدمام، طريق الأمير نايف بن عبد العزيز</span></p>
+                    <p class="font-bold py-1">الهاتف: <span class="font-normal">+966 11 400 0085</span></p>
+                    <p class="font-bold py-1">البريد الإلكتروني: <span class="font-normal">admin@admin.com</span></p>
                 </div>
             </div>
 
        </div>
 
-        <div class="copy-right w-[100%] h-[2rem] flex justify-center items-center ">
-            <h1 class="text-[1rem] text-[#efc02e]">Copy Rights Reserved © 2024 AL Simail Co</h1>
+        <div class="copy-right w-[100%] h-[2rem] flex justify-center items-center">
+            <h1 class="text-[1rem] text-[#efc02e]">جميع الحقوق محفوظة © 2024 شركة السماعيل</h1>
         </div>
     </div>
 
-        <!--mobile version-->
+    <!--mobile version-->
 </section>
--- resources/views/ar/sections/services.blade.php
+++ resources/views/ar/sections/services.blade.php
@@ -1,105 +1,89 @@
-<section id="services" class="w-[100vw] h-[auto] border-t-2 border-[#efc02e] mb-[4rem]">
+<section id="services" class="w-[100vw] h-[auto] border-t-2 border-[#efc02e] mb-[4rem]" dir="rtl">
     <div class="w-[100vw] h-[auto] my-[5rem] flex justify-center">
         <h1 class="text-[#efc02e] md:text-[4rem] text-[2.5rem] font-bold">خدماتنا</h1>
     </div>
     <div class="container m-auto py-10 flex justify-center items-center bg-[#efc02e] rounded-3xl flex-wrap">
         
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/9.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]"> Road planning works </h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">أعمال تخطيط الطرق</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/7.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Asphalt Quality Testing</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">اختبار جودة الأسفلت</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/5.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]"> Asphalt Production </h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">إنتاج الأسفلت</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/2.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Heating Systems</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">أنظمة التسخين</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/11.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Plaster Supply works </h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">أعمال توريد الجبس</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/3.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Material Production</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">إنتاج المواد</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/12.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Crushers</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">الكسارات</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/4.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Stone Production</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">إنتاج الحجر</h1>
             </div>
-            
         </div>
 
         <div class="card w-[25rem] h-[25rem] m-10 p-4 bg-white rounded-lg flex flex-col justify-around items-center">
-        <div class="w-[100%] h-[70%]  ">
+            <div class="w-[100%] h-[70%]">
                 <img class="rounded-lg w-[100%] h-[100%]" src="{{asset('images/13.png')}}" alt="" loading="lazy">
             </div> 
             <div class="w-[100%] h-[25%] flex justify-center items-center">
-                <h1 class="md:text-[2rem] text-[1.5rem]  font-bold text-[#1e304e]">Digging Works</h1>
+                <h1 class="md:text-[2rem] text-[1.5rem] font-bold text-[#1e304e]">أعمال الحفر</h1>
             </div>
-            
         </div>
 
-
-      
-
-       
-
-       
-        
     </div>
 </section>
