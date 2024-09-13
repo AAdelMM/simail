5ef15de8f147215cdf58b45ae277bf98f032ff47
AAdelMM <ahmed.adel426@gmail.com>
2024-09-13 23:17:12 +0300

    change client images

--- resources/views/ar/sections/clinets.blade.php
+++ resources/views/ar/sections/clinets.blade.php
@@ -4,9 +4,32 @@
         <div class="w-1/2 flex flex-col items-center justify-center my-10">
             <h1 class="md:text-[4rem] text-[2rem] text-[#efc02e] font-bold ">عملاؤنا</h1>
             <div class="w-1/2  border-t-4 border-[#efc02e]"></div>
+            
         </div>
-        <div class="clinets-gallary my-10 flex flex-wrap justify-center items center">
-            <div class="logo w-[70vw]  h-[auto] bg-[#efc02e] rounded-3xl flex justify-center p-4"><img class="w-[90%]" src="{{asset('images/test1.png')}}" alt=""></div>
+        <div class="clinets-gallary w-[70vw] my-10 flex flex-wrap justify-center items center bg-[#efc02e] rounded-3xl">
+           
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/1.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/2.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/3.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/4.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/5.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/6.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/7.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/8.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/9.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/10.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/11.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/12.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/13.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/14.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/15.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/16.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/17.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/18.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/19.svg') }}" alt="client image"></div>
+            <div class="w-[20%] h-auto my-[-2rem]"><img src="{{ asset('images/20.svg') }}" alt="client image"></div>
+
+            <!-- <div class="logo w-[70vw]  h-[auto] bg-[#efc02e] rounded-3xl flex justify-center p-4"><img class="w-[90%]" src="{{asset('images/test1.png')}}" alt=""></div> -->
         </div>
     </div>
 
